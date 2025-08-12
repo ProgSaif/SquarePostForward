c35a091e011e221a309de1125e1fa6b83559a2f0
ProgSaif <isaifulone@gmail.com>
2025-08-13 02:12:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 # Filter configurations
-FORBIDDEN_WORDS = ['big', 'box', '#square', '#slot', 'thxbox', 'thx', 'angelia']
+FORBIDDEN_WORDS = ['@', 'big', 'box', '#square', '#slot', 'thxbox', 'thx', 'angelia']
 VALID_NUMBERS = ['USDT', 'Answer:', '#square', '𝑩𝒊𝒏𝒂𝒏𝒄𝒆 𝑭𝒆𝒆𝒅', '𝑨𝒏𝒔𝒘𝒆𝒓 :']
-BINANCE_LINK_PATTERN = re.compile(r'(https://app\.binance\.com/uni-qr/cpos/\d+)')
+BINANCE_LINK_PATTERN = re.compile(r'(https://app\.binance\.com/uni-qr/(?:cpos|cart)/\d+)')
 
 class ForwarderBot:
     def __init__(self):
