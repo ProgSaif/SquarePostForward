4feba6b664ba39c1fb6e4a34c264a30dba2b61ec
ProgSaif <isaifulone@gmail.com>
2025-11-26 19:31:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ class ForwarderBot:
                 formatted_message += f"𝑨𝒏𝒔𝒘𝒆𝒓 : `{answer_text}`\n\n﹍﹍﹍﹍﹍﹍﹍﹍﹍﹍﹍﹍\n"
             
                                     
-                formatted_message += f"                𝑨𝒅𝒔\n➲ @BinanceSquare_Hub  \n#Binance"
+                formatted_message += f"➲ @BinanceSquare_Hub\n➲ @BinanceRedPacket_Hub  \n#Binance"
                 
                 for target in self.target_channels:
                     try:
